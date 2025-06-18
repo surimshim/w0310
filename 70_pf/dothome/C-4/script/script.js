9f123af2f81b5f9a5b33f14a3ae40496a9aa3f9f
SSR <surim0115@gmail.com>
2025-06-19 01:00:02 +0900

    0619

---
+++ 70_pf/dothome/C-4/script/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("#tab1 li:first-of-type").click(function () {
+        $(".modal").show();
+    });
+    $(".close_btn a").click(function () {
+        $(".modal").hide();
+    });
+
+    $(".btn a").click(function () {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+
+        var idx = $(this).index();
+        $(".tabs div").eq(idx).addClass("active");
+        $(".tabs div").eq(idx).siblings("div").removeClass("active");
+    });
+});
