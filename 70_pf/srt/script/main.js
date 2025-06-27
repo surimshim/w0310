a550ce6014d7f3b0bcbfb2fda2d1e118a359590c
Surim Shim <surim0115@gmail.com>
2025-06-27 16:28:02 +0900

    0627

--- 70_pf/srt/script/main.js
+++ 70_pf/srt/script/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // 풀페이지 구현
     let now = 0;
-    const sections =$('.container > section');
-    const headerHeight =$('#header').height();
+    const sections = $(".container > section");
+    const headerHeight = $("#header").height();
     const total = sections.length + 1;
 
     function scrollTo(index) {
@@ -10,13 +10,13 @@ $(document).ready(function() {
         if (index < sections.length) {
             pos = sections.eq(index).offset().top - headerHeight;
         } else {
-            pos = $('#footer').offset().top - headerHeight;
+            pos = $("#footer").offset().top - headerHeight;
         }
-        $('html,body').stop().animate({ scrollTop: pos }, 500);
+        $("html,body").stop().animate({ scrollTop: pos }, 500);
     }
 
-    $(window).on('wheel', function (e) {
-        if ($('html, body').is(':animated')) return; // 중복 스크롤 방지
+    $(window).on("wheel", function (e) {
+        if ($("html, body").is(":animated")) return; // 중복 스크롤 방지
         const delta = e.originalEvent.deltaY;
         if (delta > 0 && now < total - 1) {
             now++;
@@ -27,12 +27,11 @@ $(document).ready(function() {
     });
 
     // 스크롤 시 헤더 고정
-    $(window).on('scroll', function() {
+    $(window).on("scroll", function () {
         if ($(window).scrollTop() > 50) {
-            $('#header').addClass('scrolled');
-        }
-        else {
-            $('#header').removeClass('scrolled');
+            $("#header").addClass("scrolled");
+        } else {
+            $("#header").removeClass("scrolled");
         }
     });
 
@@ -43,21 +42,21 @@ $(document).ready(function() {
         $(".language_list").toggle();
     });
 
-    $(document).click(function(){
+    $(document).click(function () {
         $(".language_list").hide();
     });
 
     // 섹션1 - 가로 슬라이더
     setInterval(function () {
-        $('#slider ul').animate({left: '-=100%'}, 1000, function () {
-            $(this).append($(this).children().first()).css('left', 0);
+        $("#slider ul").animate({ left: "-=100%" }, 1000, function () {
+            $(this).append($(this).children().first()).css("left", 0);
         });
     }, 5000);
 
     // 섹션1 - 탭 버튼
     $(".bookingtab_btn a").click(function (event) {
         event.preventDefault();
-        
+
         // 2. 버튼에 .active 클래스 추가
         $(this).addClass("active");
         // 3. this 형제의 .active 클래스 제거
@@ -68,7 +67,10 @@ $(document).ready(function() {
         // 5. idx 순번에 해당하는 div 보여줌
         $("#booking_tabs section").eq(idx).addClass("active");
         // 6. idx 순번에 해당하는 div 형제 숨김
-        $("#booking_tabs section").eq(idx).siblings("section").removeClass("active");
+        $("#booking_tabs section")
+            .eq(idx)
+            .siblings("section")
+            .removeClass("active");
     });
 
     // 섹션2 캐로셀
@@ -163,7 +165,9 @@ $(document).ready(function() {
         // 모든 탭 컨텐츠 숨기기
         $("#section2 .tour_carousel_container").removeClass("active");
 
-        const activeTabContent = $("#section2 .tour_carousel_container").eq(index);
+        const activeTabContent = $("#section2 .tour_carousel_container").eq(
+            index
+        );
         // 해당 인덱스의 컨텐츠 보이기
         activeTabContent.addClass("active");
 
