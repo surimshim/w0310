891c2b6eb6bf6565c54100bca8bbd596e214ca20
Surim Shim <surim0115@gmail.com>
2025-08-11 15:24:20 +0900

    0811

--- 70_pf/stanley - 복사본/script/FullPage.js
+++
@@ -1,7 +0,0 @@
-new fullpage('#fullpage', {
-    autoScrolling: true,
-    navigation: true,
-    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
-    navigationTooltips: ['First', 'Second', 'Third', 'Fourth'],
-    showActiveTooltip: true,
-});
